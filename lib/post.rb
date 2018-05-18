a08a1882da30ee58fb7d28e232b66ce62c46ffe7
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-18 02:55:46 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,9 +1,9 @@
 class Post
   attr_accessor :title, :author
 
-  def initialize(title)
-    @title = title
-    author = title
-  end
+  # def initialize(title)
+  #   @title = title
+  #   author = title
+  # end
 
 end
